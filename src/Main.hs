a79129f12e77b2ebf6679ee87e9499b3f1f8c943
Albert Goncalves <goncalves.albert@gmail.com>
2021-07-31 10:47:58 -0400

    day 7, part 1

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@
 import Control.Applicative (liftA2)
 import Control.Exception (assert)
 import Data.Either (lefts, rights)
+import Data.List (foldl', permutations)
 import Data.Text (Text, splitOn)
 import Data.Text.IO (readFile)
 import Data.Text.Read (decimal, signed)
@@ -11,6 +12,11 @@ import System.Environment (getArgs)
 import Test (Loc (..), test)
 import Vm (Vm (..), run)
 import Prelude hiding (readFile)
+
+chain :: [Int] -> [Int] -> Int
+chain xs = foldl' f 0
+  where
+    f in1 in0 = head $ getOutput $ run $ Vm xs [] [in0, in1] []
 
 #define TEST test (Loc (__FILE__, __LINE__))
 
@@ -32,6 +38,83 @@ tests = do
   TEST (f1 ([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9], [-9])) 1
   TEST (f1 ([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9], [0])) 0
   TEST (f1 ([3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1], [-1])) 1
+  TEST
+    ( chain
+        [3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0]
+        [4, 3, 2, 1, 0]
+    )
+    43210
+  TEST
+    ( chain
+        [ 3,
+          23,
+          3,
+          24,
+          1002,
+          24,
+          10,
+          24,
+          1002,
+          23,
+          -1,
+          23,
+          101,
+          5,
+          23,
+          23,
+          1,
+          24,
+          23,
+          23,
+          4,
+          23,
+          99,
+          0,
+          0
+        ]
+        [0, 1, 2, 3, 4]
+    )
+    54321
+  TEST
+    ( chain
+        [ 3,
+          31,
+          3,
+          32,
+          1002,
+          32,
+          10,
+          32,
+          1001,
+          31,
+          -2,
+          31,
+          1007,
+          31,
+          0,
+          33,
+          1002,
+          33,
+          7,
+          33,
+          1,
+          33,
+          31,
+          31,
+          1,
+          32,
+          31,
+          31,
+          4,
+          31,
+          99,
+          0,
+          0,
+          0
+        ]
+        [1, 0, 4, 3, 2]
+    )
+    65210
   where
     f0 = getProgram . run . (\xs -> Vm xs [] [] [])
     f1 = head . getOutput . run . (\(xs, input) -> Vm xs [] input [])
@@ -60,6 +143,11 @@ solve5 xs = do
   TEST (getOutput $ run $ Vm xs [] [1] []) [5821753, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   TEST (getOutput $ run $ Vm xs [] [5] []) [11956381]
 
+-- NOTE: See `https://adventofcode.com/2019/day/7`.
+solve7 :: [Int] -> IO ()
+solve7 xs = do
+  TEST (maximum $ map (chain xs) $ permutations [0 .. 4]) 199988
+
 intoProgram :: Text -> [Int]
 intoProgram x = assert (null $ lefts xs) $ rights xs
   where
@@ -69,11 +157,12 @@ intoProgram x = assert (null $ lefts xs) $ rights xs
 main :: IO ()
 main = do
   addNewline tests
-  [path2, path5] <- getArgs
+  [path2, path5, path7] <- getArgs
   mapM_
     (\(f, x) -> addNewline . f . intoProgram =<< readFile x)
     [ (solve2, path2),
-      (solve5, path5)
+      (solve5, path5),
+      (solve7, path7)
     ]
   where
     addNewline :: IO () -> IO ()
