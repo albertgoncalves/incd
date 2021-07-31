efd033fc91426fef8f19127cf6dac4fdc74499e3
Albert Goncalves <goncalves.albert@gmail.com>
2021-07-31 12:25:50 -0400

    solve day 7 part 2

--- src/Main.hs
+++ src/Main.hs
@@ -13,12 +13,32 @@ import Test (Loc (..), test)
 import Vm (Vm (..), VmState (..), newState, run)
 import Prelude hiding (readFile)
 
-chain :: [Int] -> [Int] -> Int
-chain xs = foldl' f 0
+chain0 :: [Int] -> [Int] -> Int
+chain0 xs = foldl' f 0
   where
     f in1 in0 =
       head $
         getOutput $ getState $ run $ Vm xs [] (VmState True 0 [in0, in1] [])
+
+appendInput :: VmState -> Int -> VmState
+appendInput (VmState alive index input output) in1 =
+  VmState alive index (input ++ [in1]) output
+
+chain1 :: [Int] -> [Int] -> Int
+chain1 xs inputs =
+  loop (map (\in0 -> Vm xs [] (VmState True 0 [in0] [])) inputs) 0
+  where
+    loop :: [Vm] -> Int -> Int
+    loop ((Vm program _ state) : vms) in1 =
+      case run $ Vm program [] $ appendInput state in1 of
+        (Vm _ _ (VmState False _ _ [out0])) ->
+          case vms of
+            [] -> out0
+            vms' -> loop vms' out0
+        (Vm program' _ (VmState True index' input' [out0])) ->
+          loop (vms ++ [Vm program' [] $ VmState True index' input' []]) out0
+        _ -> undefined
+    loop _ _ = undefined
 
 #define TEST test (Loc (__FILE__, __LINE__))
 
@@ -41,13 +61,13 @@ tests = do
   TEST (f1 ([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9], [0])) 0
   TEST (f1 ([3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1], [-1])) 1
   TEST
-    ( chain
+    ( chain0
         [3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0]
         [4, 3, 2, 1, 0]
     )
     43210
   TEST
-    ( chain
+    ( chain0
         [ 3,
           23,
           3,
@@ -78,7 +98,7 @@ tests = do
     )
     54321
   TEST
-    ( chain
+    ( chain0
         [ 3,
           31,
           3,
@@ -117,6 +137,104 @@ tests = do
         [1, 0, 4, 3, 2]
     )
     65210
+  TEST
+    ( chain1
+        [ 3,
+          26,
+          1001,
+          26,
+          -4,
+          26,
+          3,
+          27,
+          1002,
+          27,
+          2,
+          27,
+          1,
+          27,
+          26,
+          27,
+          4,
+          27,
+          1001,
+          28,
+          -1,
+          28,
+          1005,
+          28,
+          6,
+          99,
+          0,
+          0,
+          5
+        ]
+        [9, 8, 7, 6, 5]
+    )
+    139629729
+  TEST
+    ( chain1
+        [ 3,
+          52,
+          1001,
+          52,
+          -5,
+          52,
+          3,
+          53,
+          1,
+          52,
+          56,
+          54,
+          1007,
+          54,
+          5,
+          55,
+          1005,
+          55,
+          26,
+          1001,
+          54,
+          -5,
+          54,
+          1105,
+          1,
+          12,
+          1,
+          53,
+          54,
+          53,
+          1008,
+          54,
+          0,
+          55,
+          1001,
+          55,
+          1,
+          55,
+          2,
+          53,
+          55,
+          53,
+          4,
+          53,
+          1001,
+          56,
+          -1,
+          56,
+          1005,
+          56,
+          6,
+          99,
+          0,
+          0,
+          0,
+          0,
+          10
+        ]
+        [9, 7, 8, 5, 6]
+    )
+    18216
   where
     f0 = getProgram . run . (\xs -> Vm xs [] newState)
     f1 =
@@ -157,7 +275,8 @@ solve5 xs = do
 -- NOTE: See `https://adventofcode.com/2019/day/7`.
 solve7 :: [Int] -> IO ()
 solve7 xs = do
-  TEST (maximum $ map (chain xs) $ permutations [0 .. 4]) 199988
+  TEST (maximum $ map (chain0 xs) $ permutations [0 .. 4]) 199988
+  TEST (maximum $ map (chain1 xs) $ permutations [5 .. 9]) 17519904
 
 intoProgram :: Text -> [Int]
 intoProgram x = assert (null $ lefts xs) $ rights xs
