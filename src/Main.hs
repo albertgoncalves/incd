f055ffad38816d20d6d97b91f61e2fd7cceb9159
Albert Goncalves <goncalves.albert@gmail.com>
2021-07-31 23:44:43 -0400

    solve day 9

--- src/Main.hs
+++ src/Main.hs
@@ -18,20 +18,24 @@ chain0 program = foldl' f 0
   where
     f in1 in0 =
       head $
-        getOutput $ getState $ run $ Vm program (VmState True 0 [in0, in1] [])
+        getOutput $
+          getState $
+            run (length program - 1) $
+              Vm program $
+                foldl' appendInput newState [in0, in1]
 
 chain1 :: [Int] -> [Int] -> Int
 chain1 program inputs = loop (map (Vm program . appendInput newState) inputs) 0
   where
     loop :: [Vm] -> Int -> Int
     loop ((Vm program' state) : vms) in1 =
-      case run $ Vm program' $ appendInput state in1 of
-        (Vm _ (VmState False _ _ [out0])) ->
+      case run (length program - 1) $ Vm program' $ appendInput state in1 of
+        (Vm _ (VmState False _ _ _ [out0])) ->
           case vms of
             [] -> out0
             vms' -> loop vms' out0
-        (Vm program'' (VmState True index' input' [out0])) ->
-          loop (vms ++ [Vm program'' $ VmState True index' input' []]) out0
+        (Vm program'' (VmState True index base input [out0])) ->
+          loop (vms ++ [Vm program'' $ VmState True index base input []]) out0
         _ -> undefined
     loop _ _ = undefined
 
@@ -39,22 +43,22 @@ chain1 program inputs = loop (map (Vm program . appendInput newState) inputs) 0
 
 tests :: IO ()
 tests = do
-  TEST (f0 [1, 0, 0, 0, 99]) [2, 0, 0, 0, 99]
-  TEST (f0 [2, 3, 0, 3, 99]) [2, 3, 0, 6, 99]
-  TEST (f0 [2, 4, 4, 5, 99, 0]) [2, 4, 4, 5, 99, 9801]
-  TEST (f0 [1, 1, 1, 4, 99, 5, 6, 0, 99]) [30, 1, 1, 4, 2, 5, 6, 0, 99]
-  TEST (f0 [1002, 4, 3, 4, 33]) [1002, 4, 3, 4, 99]
-  TEST (f0 [1101, 100, -1, 4, 0]) [1101, 100, -1, 4, 99]
-  TEST (f1 ([3, 0, 4, 0, 99], [1234])) 1234
-  TEST (f1 ([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], [8])) 1
-  TEST (f1 ([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], [7])) 0
-  TEST (f1 ([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], [7])) 1
-  TEST (f1 ([3, 3, 1108, -1, 8, 3, 4, 3, 99], [8])) 1
-  TEST (f1 ([3, 3, 1108, -1, 8, 3, 4, 3, 99], [9])) 0
-  TEST (f1 ([3, 3, 1107, -1, 8, 3, 4, 3, 99], [7])) 1
-  TEST (f1 ([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9], [-9])) 1
-  TEST (f1 ([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9], [0])) 0
-  TEST (f1 ([3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1], [-1])) 1
+  TEST (f0 4 [1, 0, 0, 0, 99]) [2, 0, 0, 0, 99]
+  TEST (f0 4 [2, 3, 0, 3, 99]) [2, 3, 0, 6, 99]
+  TEST (f0 5 [2, 4, 4, 5, 99, 0]) [2, 4, 4, 5, 99, 9801]
+  TEST (f0 8 [1, 1, 1, 4, 99, 5, 6, 0, 99]) [30, 1, 1, 4, 2, 5, 6, 0, 99]
+  TEST (f0 4 [1002, 4, 3, 4, 33]) [1002, 4, 3, 4, 99]
+  TEST (f0 4 [1101, 100, -1, 4, 0]) [1101, 100, -1, 4, 99]
+  TEST (f1 [3, 0, 4, 0, 99] [1234]) [1234]
+  TEST (f1 [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8] [8]) [1]
+  TEST (f1 [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8] [7]) [0]
+  TEST (f1 [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8] [7]) [1]
+  TEST (f1 [3, 3, 1108, -1, 8, 3, 4, 3, 99] [8]) [1]
+  TEST (f1 [3, 3, 1108, -1, 8, 3, 4, 3, 99] [9]) [0]
+  TEST (f1 [3, 3, 1107, -1, 8, 3, 4, 3, 99] [7]) [1]
+  TEST (f1 [3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9] [-9]) [1]
+  TEST (f1 [3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9] [0]) [0]
+  TEST (f1 [3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1] [-1]) [1]
   TEST
     ( chain0
         [3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0]
@@ -230,14 +234,37 @@ tests = do
         [9, 7, 8, 5, 6]
     )
     18216
+  TEST
+    ( take 16 $
+        f0
+          1008
+          [ 109,
+            1,
+            204,
+            -1,
+            1001,
+            100,
+            1,
+            100,
+            1008,
+            100,
+            16,
+            101,
+            1006,
+            101,
+            0,
+            99
+          ]
+    )
+    [109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99]
+  TEST (f1 [1102, 34915192, 34915192, 7, 4, 7, 99, 0] []) [1219070632396864]
+  TEST (f1 [104, 1125899906842624, 99] []) [1125899906842624]
   where
-    f0 = getProgram . run . (`Vm` newState)
-    f1 =
-      head
-        . getOutput
-        . getState
-        . run
-        . (\(program, input) -> Vm program (VmState True 0 input []))
+    f0 cap program = getProgram $ run cap $ Vm program newState
+    f1 program input =
+      getOutput $
+        getState $
+          run (length program - 1) $ Vm program $ VmState True 0 0 input []
 
 -- NOTE: See `https://adventofcode.com/2019/day/2`.
 solve2 :: [Int] -> IO ()
@@ -253,7 +280,12 @@ solve2 program = do
     )
     2552
   where
-    f = head . getProgram . run . (`Vm` newState) . patch program
+    f =
+      head
+        . getProgram
+        . run (length program - 1)
+        . (`Vm` newState)
+        . patch program
 
 -- NOTE: See `https://adventofcode.com/2019/day/5`.
 solve5 :: [Int] -> IO ()
@@ -261,7 +293,10 @@ solve5 program = do
   TEST (f 1) [5821753, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   TEST (f 5) [11956381]
   where
-    f in0 = getOutput $ getState $ run $ Vm program $ VmState True 0 [in0] []
+    f in0 =
+      getOutput $
+        getState $
+          run (length program - 1) $ Vm program $ appendInput newState in0
 
 -- NOTE: See `https://adventofcode.com/2019/day/7`.
 solve7 :: [Int] -> IO ()
@@ -270,6 +305,14 @@ solve7 program = do
   TEST (f chain1 [5 .. 9]) 17519904
   where
     f c = maximum . map (c program) . permutations
+
+-- NOTE: See `https://adventofcode.com/2019/day/9`.
+solve9 :: [Int] -> IO ()
+solve9 program = do
+  TEST (f 1) [4234906522]
+  TEST (f 2) [60962]
+  where
+    f = getOutput . getState . run 4323 . Vm program . appendInput newState
 
 intoProgram :: Text -> [Int]
 intoProgram x = assert (null $ lefts xs) $ rights xs
@@ -280,12 +323,13 @@ intoProgram x = assert (null $ lefts xs) $ rights xs
 main :: IO ()
 main = do
   addNewline tests
-  [path2, path5, path7] <- getArgs
+  [path2, path5, path7, path9] <- getArgs
   mapM_
     (\(f, x) -> addNewline . f . intoProgram =<< readFile x)
     [ (solve2, path2),
       (solve5, path5),
-      (solve7, path7)
+      (solve7, path7),
+      (solve9, path9)
     ]
   where
     addNewline :: IO () -> IO ()
--- src/Vm.hs
+++ src/Vm.hs
@@ -12,6 +12,7 @@ import Data.Array.MArray (MArray, newListArray, readArray, writeArray)
 data VmState = VmState
   { getAlive :: Bool,
     getIndex :: Int,
+    getBase :: Int,
     getInput :: [Int],
     getOutput :: [Int]
   }
@@ -22,97 +23,102 @@ data Vm = Vm
   }
 
 newState :: VmState
-newState = VmState True 0 [] []
+newState = VmState True 0 0 [] []
 
 appendInput :: VmState -> Int -> VmState
-appendInput (VmState alive index input output) in1 =
-  VmState alive index (input ++ [in1]) output
+appendInput (VmState alive index base input output) in1 =
+  VmState alive index base (input ++ [in1]) output
 
-readVal :: (MArray a Int m) => a Int Int -> Int -> Int -> m Int
-readVal array 0 = readArray array <=< readArray array
-readVal array 1 = readArray array
-readVal _ _ = undefined
+readVal :: (MArray a Int m) => a Int Int -> Int -> Int -> Int -> m Int
+readVal array 0 _ = readArray array <=< readArray array
+readVal array 1 _ = readArray array
+readVal array 2 base = readArray array . (base +) <=< readArray array
+readVal _ _ _ = undefined
 
-readDst :: (MArray a Int m) => a Int Int -> Int -> Int -> m Int
-readDst array 0 = readArray array
-readDst _ _ = undefined
+readDst :: (MArray a Int m) => a Int Int -> Int -> Int -> Int -> m Int
+readDst array 0 _ = readArray array
+readDst array 2 base = fmap (base +) . readArray array
+readDst _ _ _ = undefined
 
 loop :: (MArray a Int m) => a Int Int -> VmState -> m VmState
-loop array (VmState _ i input output) = do
+loop array (VmState _ i base input output) = do
   x <- readArray array i
   let m0 = (x `div` 100) `mod` 10
       m1 = (x `div` 1000) `mod` 10
       m2 = (x `div` 10000) `mod` 10
   case x `mod` 100 of
     1 -> do
-      val0 <- readVal array m0 (i + 1)
-      val1 <- readVal array m1 (i + 2)
-      dst <- readDst array m2 (i + 3)
+      val0 <- readVal array m0 base (i + 1)
+      val1 <- readVal array m1 base (i + 2)
+      dst <- readDst array m2 base (i + 3)
       writeArray array dst (val0 + val1)
-      loop array (VmState True (i + 4) input output)
+      loop array $ VmState True (i + 4) base input output
     2 -> do
-      val0 <- readVal array m0 (i + 1)
-      val1 <- readVal array m1 (i + 2)
-      dst <- readDst array m2 (i + 3)
+      val0 <- readVal array m0 base (i + 1)
+      val1 <- readVal array m1 base (i + 2)
+      dst <- readDst array m2 base (i + 3)
       writeArray array dst (val0 * val1)
-      loop array (VmState True (i + 4) input output)
+      loop array $ VmState True (i + 4) base input output
     3 -> case input of
       (val : input') -> do
-        dst <- readDst array m0 (i + 1)
+        dst <- readDst array m0 base (i + 1)
         writeArray array dst val
-        loop array (VmState True (i + 2) input' output)
-      [] -> return $ VmState True i [] output
+        loop array $ VmState True (i + 2) base input' output
+      [] -> return $ VmState True i base [] output
     4 -> do
-      val <- readVal array m0 (i + 1)
-      loop array (VmState True (i + 2) input (val : output))
+      val <- readVal array m0 base (i + 1)
+      loop array $ VmState True (i + 2) base input (val : output)
     5 -> do
-      val0 <- readVal array m0 (i + 1)
-      val1 <- readVal array m1 (i + 2)
+      val0 <- readVal array m0 base (i + 1)
+      val1 <- readVal array m1 base (i + 2)
       if val0 /= 0
-        then loop array (VmState True val1 input output)
-        else loop array (VmState True (i + 3) input output)
+        then loop array $ VmState True val1 base input output
+        else loop array $ VmState True (i + 3) base input output
     6 -> do
-      val0 <- readVal array m0 (i + 1)
-      val1 <- readVal array m1 (i + 2)
+      val0 <- readVal array m0 base (i + 1)
+      val1 <- readVal array m1 base (i + 2)
       if val0 == 0
-        then loop array (VmState True val1 input output)
-        else loop array (VmState True (i + 3) input output)
+        then loop array $ VmState True val1 base input output
+        else loop array $ VmState True (i + 3) base input output
     7 -> do
-      val0 <- readVal array m0 (i + 1)
-      val1 <- readVal array m1 (i + 2)
-      dst <- readDst array m2 (i + 3)
+      val0 <- readVal array m0 base (i + 1)
+      val1 <- readVal array m1 base (i + 2)
+      dst <- readDst array m2 base (i + 3)
       writeArray array dst $
         if val0 < val1
           then 1
           else 0
-      loop array (VmState True (i + 4) input output)
+      loop array $ VmState True (i + 4) base input output
     8 -> do
-      val0 <- readVal array m0 (i + 1)
-      val1 <- readVal array m1 (i + 2)
-      dst <- readDst array m2 (i + 3)
+      val0 <- readVal array m0 base (i + 1)
+      val1 <- readVal array m1 base (i + 2)
+      dst <- readDst array m2 base (i + 3)
       writeArray array dst $
         if val0 == val1
           then 1
           else 0
-      loop array (VmState True (i + 4) input output)
-    99 -> return $ VmState False i input output
+      loop array $ VmState True (i + 4) base input output
+    9 -> do
+      val <- readVal array m0 base (i + 1)
+      loop array $ VmState True (i + 2) (base + val) input output
+    99 -> return $ VmState False i base input output
     _ -> undefined
 
-toArray :: (MArray a Int m) => [Int] -> m (a Int Int)
-toArray xs = newListArray (0, length xs - 1) xs
+toArray :: (MArray a Int m) => Int -> [Int] -> m (a Int Int)
+toArray cap = newListArray (0, cap)
 
 patch :: [Int] -> [(Int, Int)] -> [Int]
 patch program patches = runST $ do
-  array <- toArray program
+  array <- toArray (length program - 1) program
   mapM_ (uncurry $ writeArray array) patches
   elems <$> unsafeFreezeSTUArray array
 
 -- NOTE: See `https://hackage.haskell.org/package/array-0.5.4.0/docs/src/Data.Array.ST.html#runSTUArray`.
-run :: Vm -> Vm
-run vm@(Vm program state@(VmState alive _ _ _)) =
+run :: Int -> Vm -> Vm
+run cap vm@(Vm program state@(VmState alive _ _ _ _)) =
   if alive
     then runST $ do
-      array <- toArray program
+      array <- toArray cap program
       state' <- loop array state
       (`Vm` state') . elems <$> unsafeFreezeSTUArray array
     else vm
